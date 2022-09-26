b7929a59f496076c5694d5542501f5f5e0ce6e4b
SK-Bartra <islebeegun@gmail.com>
2022-09-26 17:35:09 +0300

    changed DB to local

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize('bartra_bot_db', 'root', 'YgLCoRbAMeoS', {
-  host: '109.71.12.132',
-  port: '6432',
-  dialect: 'postgres',
+module.exports = new Sequelize('bartra_bot_db', 'root', 'rootpswd', {
+  host: '127.0.0.1',
+  port: '3306',
+  dialect: 'mariadb',
 });
